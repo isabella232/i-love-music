ce34168a19ecc2a0e9ab1d21a567c7a4cb0d32bc
Filip Gałczyk <filip.galczyk13@gmail.com>
2021-09-26 13:36:21 +0200

    Props children (New layout)

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritePage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div> 
-      <MainNavigation />
-      <Switch>
-        <Route path = '/' exact>
-          <AllMeetupsPage></AllMeetupsPage>
-        </Route>
-        <Route path = '/new-meetup'>
-          <NewMeetupPage></NewMeetupPage>
-        </Route>
-        <Route path = '/favorites'>
-          <FavoritePage></FavoritePage>
-        </Route>
-      </Switch>
-    </div>
+      <Layout>
+        <Switch>
+          <Route path = '/' exact>
+            <AllMeetupsPage></AllMeetupsPage>
+          </Route>
+          <Route path = '/new-meetup'>
+            <NewMeetupPage></NewMeetupPage>
+          </Route>
+          <Route path = '/favorites'>
+            <FavoritePage></FavoritePage>
+          </Route>
+        </Switch>
+      </Layout>
   );
 }
 
