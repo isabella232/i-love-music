677f8a978a913369cd1d5e126124a5cdff930aff
Filip Gałczyk <filip.galczyk13@gmail.com>
2021-09-26 16:51:44 +0200

    Working with database (Firebase)

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
+import { useHistory } from "react-router";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage () {
+    
+    const history = useHistory();
+
+    function addMetupHandler(meetupData) {
+        fetch('https://react-course-6c662-default-rtdb.firebaseio.com/meetups.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        ).then(() => {
+            history.replace('/');
+        });
+    }
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm></NewMeetupForm>
+            <NewMeetupForm onAddMeetup={addMetupHandler}></NewMeetupForm>
         </section>
     );
 }
